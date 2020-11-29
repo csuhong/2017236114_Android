3ad9fb41c5878265b2b045c5a50bebeafb10e758
csuhong <1062051615@qq.com>
2020-11-29 18:14:30 +0800

    实验十

---
+++ 实验十 服务：启动和停止服务/ServiceTest/app/src/main/java/com/example/servicetest/MyService.java
@@ -0,0 +1,32 @@
+package com.example.servicetest;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MyService extends Service {
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        Log.d("Myservice","onCreate executed");
+    }
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId){
+        Log.d("Myservice","onStartCommand executed");
+        return super.onStartCommand(intent, flags,startId);
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d("Myservice","onDestroy executed");
+    }
+}
